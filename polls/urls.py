f0888accce090358a032e4aee808c4918a240fc9
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-10-19 00:30:40 +0530

    Initial commit

--- polls/urls.py
+++ polls/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
         path("", views.index, name="index"),
+        path("<int:question_id>/", views.detail, name="detail"),
+        path("<int:question_id>/results", views.results, name="results"),
+        path("<int:question_id>/vote", views.vote, name="vote"),
+        # you can use the last argument as reference to the url in templates or views instead of hardcoding the url
         ]
 
 
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.http import Http404
 
 def index(request):
-    return HttpResponse("Hello, world, you are at the polls index.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = ", ".join([q.question_text for q in latest_question_list])
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    return HttpResponse(request, "polls/index.html", context)
+
+def detail(request,question_id):
+    return HttpResponse("You are looking at the question %s" % question_id)
+
+def results(request, question_id):
+    response = "You are looking at the answer of questions %s"
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You are voting on the question %s" % question_id)
 
 
 
