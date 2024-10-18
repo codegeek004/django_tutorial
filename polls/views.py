a560222e8f8e836be6ca1a06afaac35a8ad9ef45
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-10-19 00:35:55 +0530

    Added detail template

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import Http404
+from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
@@ -12,7 +13,11 @@ def index(request):
     return HttpResponse(request, "polls/index.html", context)
 
 def detail(request,question_id):
-    return HttpResponse("You are looking at the question %s" % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
     response = "You are looking at the answer of questions %s"
