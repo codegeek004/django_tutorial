aafa1caf2745140308f109d113a305e453fadd01
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-10-21 14:19:52 +0530

    Issues resolved

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-qtoy(ni$v(+fv1a3v5iay2d@6knpqcqsm5b78t4(f8^ev-m)b('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 
--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.http import HttpResponseRedirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('polls/', include("polls.urls")),
+    path('', lambda request: HttpResponseRedirect('/polls/'))
 ]
